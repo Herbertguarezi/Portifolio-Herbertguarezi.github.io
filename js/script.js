2c86a3ea54f2d51678feb7a6b644f206f9935a58
Herbert Guarezi <herbertguarezi@gamil.com>
2022-12-16 15:54:19 -0300

    Adicionado mudanca de icone

--- js/script.js
+++ js/script.js
@@ -1,23 +1,35 @@
-const html = document.querySelector('html');
-const btn_modos = document.getElementById('modos');
+//muda de dark para light mode e muda os icones de modo
+const html = document.querySelector("html");
+const btn_modos = document.getElementById("modos");
 
-function troca_modo(){
-  html.classList.toggle('light-mode')
+function troca_icone() {
+  if (html.classList.contains("light-mode")) {
+    btn_modos.removeAttribute("class");
+    btn_modos.classList.add("fa-regular");
+    btn_modos.classList.add("fa-moon");
+  } else {
+    btn_modos.removeAttribute("class");
+    btn_modos.classList.add("fa-regular");
+    btn_modos.classList.add("fa-sun");
+  }
 }
-btn_modos.addEventListener('click', troca_modo);
+
+function troca_modo() {
+  html.classList.toggle("light-mode");
+  troca_icone();
+}
+btn_modos.addEventListener("click", troca_modo);
 
 //mudar o logo de acordo com o tamanho
 
-const logo = document.querySelector('.logo');
+const logo = document.querySelector(".logo");
 
-
-window.onresize = function(){
+window.onresize = function () {
   var desktop = document.querySelectorAll;
   var tamanho_tela = window.innerWidth;
-  if(tamanho_tela <= 800){
-    logo.classList.add('desktop')
+  if (tamanho_tela <= 800) {
+    logo.classList.add("desktop");
+  } else {
+    logo.classList.remove("desktop");
   }
-  else{
-    logo.classList.remove('desktop')
-  }
-}
+};

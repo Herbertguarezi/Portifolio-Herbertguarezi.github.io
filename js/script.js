fcc3145deca5c86172067dd722a30a206c5929ab
Herbert Guarezi <herbertguarezi@gamil.com>
2022-12-16 14:31:23 -0300

    Adicionado media

--- js/script.js
+++ js/script.js
@@ -4,9 +4,20 @@ const btn_modos = document.getElementById('modos');
 function troca_modo(){
   html.classList.toggle('light-mode')
 }
-
-console.log('Bem vindo');
-
 btn_modos.addEventListener('click', troca_modo);
 
-console.log('teste');
+//mudar o logo de acordo com o tamanho
+
+const logo = document.querySelector('.logo');
+
+
+window.onresize = function(){
+  var desktop = document.querySelectorAll;
+  var tamanho_tela = window.innerWidth;
+  if(tamanho_tela <= 800){
+    logo.classList.add('desktop')
+  }
+  else{
+    logo.classList.remove('desktop')
+  }
+}
